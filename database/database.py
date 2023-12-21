3e4cfa88492c912d106fb81d3674315b21aa010a
Guangyu He <fredhgy2010@gmail.com>
2023-12-21 22:15:44 +0100

    fix ```check_same_thread``` error

--- database/database.py
+++ database/database.py
@@ -2,12 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+"""
+LOCAL DATABASE
+"""
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:example@postgres/postgres"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+"""
+POSTGRES DATABASE
+"""
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:example@postgres/postgres"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
