9df0d77f3a50846ccd0ea242776286ed463fa2d4
Guangyu He <fredhgy2010@gmail.com>
2023-12-22 13:14:57 +0100

    set aes key slice and minor refactor

--- aes/aes_encrypt.py
+++ aes/aes_encrypt.py
@@ -5,15 +5,15 @@ from Crypto.Util.Padding import pad, unpad
 
 
 def aes_encrypt(plaintext, key) -> dict:
-    cipher = AES.new(key.encode("utf-8"), AES.MODE_CBC)
+    cipher = AES.new(key[:32].encode("utf-8"), AES.MODE_CBC)
     ciphertext = cipher.encrypt(pad(plaintext.encode("utf-8"), AES.block_size))
     iv = base64.b64encode(cipher.iv).decode("utf-8")
     encrypted_text = base64.b64encode(ciphertext).decode("utf-8")
-    return {"iv": iv, "encrypted_text": encrypted_text}
+    return {"iv": iv, "text": encrypted_text}
 
 
 def aes_decrypt(iv, encrypted_text, key):
-    cipher = AES.new(key.encode("utf-8"), AES.MODE_CBC, base64.b64decode(iv))
+    cipher = AES.new(key[:32].encode("utf-8"), AES.MODE_CBC, base64.b64decode(iv))
     decrypted_text = unpad(cipher.decrypt(base64.b64decode(encrypted_text)), AES.block_size)
     return decrypted_text.decode("utf-8")
 
